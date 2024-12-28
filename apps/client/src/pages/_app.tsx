df5cda87ddc3f69992281b3d73f2c89495c40013
Om Gupta <omgupta0720@gmail.com>
2024-12-28 22:54:14 +0530

    add: loading

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,13 +1,12 @@
-import { GeistSans } from "geist/font/sans";
-import { type AppType } from "next/app";
+import type { AppType } from 'next/app';
 
-import { api } from "@/utils/api";
+import { api } from '@/utils/api';
 
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
+    <div className="font-mono">
       <Component {...pageProps} />
     </div>
   );
